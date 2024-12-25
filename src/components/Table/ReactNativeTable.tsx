300b91b0658c88746cf7a096efb6f603adb7bb8e
joharkhan99 <joharkhan1999@gmail.com>
2024-12-25 18:51:48 +0500

    added basic pagination structure

--- src/components/Table/ReactNativeTable.tsx
+++ src/components/Table/ReactNativeTable.tsx
@@ -9,135 +9,189 @@ import {
 
 const ReactNativeTable = () => {
   return (
-    <View style={{flex: 1, backgroundColor: 'white', paddingHorizontal: 4}}>
-      <View
-        style={{
-          flex: 1,
-          flexDirection: 'row',
-          justifyContent: 'flex-start',
-        }}>
-        <View style={{flex: 1, width: Dimensions.get('screen').width}}>
-          <ScrollView
-            horizontal
-            contentContainerStyle={{flexDirection: 'column'}}
-            // ref={scrollRef}
-          >
-            <View
-              style={{
-                flexDirection: 'column',
-                alignItems: 'flex-start',
-                rowGap: 2,
-              }}>
+    <ScrollView
+      style={{
+        flex: 1,
+        backgroundColor: 'white',
+      }}>
+      <View style={{padding: 10}}>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'flex-start',
+          }}>
+          <View style={{flex: 1, width: Dimensions.get('screen').width}}>
+            <ScrollView
+              horizontal
+              contentContainerStyle={{flexDirection: 'column'}}
+              // ref={scrollRef}
+            >
               <View
-                // className="flex-row justify-start items-end w-full gap-x-2"
                 style={{
-                  flexDirection: 'row',
-                  justifyContent: 'flex-start',
-                  alignItems: 'flex-end',
-                  columnGap: 10,
+                  flexDirection: 'column',
+                  alignItems: 'flex-start',
+                  rowGap: 2,
                 }}>
-                {/* header/column */}
                 <View
-                  // key={`subColumn-${index}`}
                   style={{
-                    alignItems: 'flex-start',
                     flexDirection: 'row',
-                    width: 100,
-                  }}>
-                  <TouchableOpacity
-                    style={{
-                      flexDirection: 'row',
-                      alignItems: 'center',
-                      justifyContent: 'flex-start',
-                      columnGap: 1,
-                    }}>
-                    <Text>headerName</Text>
-                    <Text
-                      style={{
-                        transform: 'rotate(-90deg)',
-                      }}>
-                      ➜
-                    </Text>
-                  </TouchableOpacity>
-                </View>
-
-                <View
-                  // key={`subColumn-${index}`}
-                  style={{
-                    alignItems: 'flex-start',
-                    flexDirection: 'row',
+                    justifyContent: 'flex-start',
+                    alignItems: 'flex-end',
                     columnGap: 10,
-                    width: 100,
                   }}>
-                  <TouchableOpacity
+                  {/* header/column */}
+                  <View
+                    // key={`subColumn-${index}`}
                     style={{
+                      alignItems: 'flex-start',
                       flexDirection: 'row',
-                      alignItems: 'center',
-                      justifyContent: 'flex-start',
-                      columnGap: 1,
+                      width: 100,
                     }}>
-                    <Text>headerName</Text>
-                    <Text
+                    <TouchableOpacity
                       style={{
-                        transform: 'rotate(-90deg)',
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        justifyContent: 'flex-start',
+                        columnGap: 1,
                       }}>
-                      ➜
-                    </Text>
-                  </TouchableOpacity>
+                      <Text>headerName</Text>
+                      <Text
+                        style={{
+                          transform: 'rotate(-90deg)',
+                        }}>
+                        ➜
+                      </Text>
+                    </TouchableOpacity>
+                  </View>
+
+                  <View
+                    // key={`subColumn-${index}`}
+                    style={{
+                      alignItems: 'flex-start',
+                      flexDirection: 'row',
+                      columnGap: 10,
+                      width: 100,
+                    }}>
+                    <TouchableOpacity
+                      style={{
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        justifyContent: 'flex-start',
+                        columnGap: 1,
+                      }}>
+                      <Text>headerName</Text>
+                      <Text
+                        style={{
+                          transform: 'rotate(-90deg)',
+                        }}>
+                        ➜
+                      </Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+
+                {/* Rows */}
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'flex-start',
+                    alignItems: 'center',
+                    columnGap: 10,
+                  }}>
+                  <View
+                    style={{
+                      width: 100,
+                    }}>
+                    <Text>Data 123</Text>
+                  </View>
+
+                  <View
+                    style={{
+                      width: 100,
+                    }}>
+                    <Text>Data 2</Text>
+                  </View>
+                </View>
+
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'flex-start',
+                    alignItems: 'center',
+                    columnGap: 10,
+                  }}>
+                  <View
+                    style={{
+                      width: 100,
+                    }}>
+                    <Text>Data 123</Text>
+                  </View>
+
+                  <View
+                    style={{
+                      width: 100,
+                    }}>
+                    <Text>Data 2</Text>
+                  </View>
                 </View>
               </View>
+            </ScrollView>
+          </View>
+        </View>
 
-              {/* Rows */}
-              <View
-                style={{
-                  flexDirection: 'row',
-                  justifyContent: 'flex-start',
-                  alignItems: 'center',
-                  columnGap: 10,
-                }}>
-                <View
-                  style={{
-                    width: 100,
-                  }}>
-                  <Text>Data 1</Text>
-                </View>
+        {/* pagination */}
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'flex-end',
+            marginVertical: 10,
+            alignItems: 'center',
+            columnGap: 5,
+          }}>
+          <View>
+            <Text>{`1 - 50 of 300`}</Text>
+          </View>
 
-                <View
-                  style={{
-                    width: 100,
-                  }}>
-                  <Text>Data 2</Text>
-                </View>
-              </View>
+          <TouchableOpacity>
+            <Text>⇽</Text>
+          </TouchableOpacity>
 
-              <View
-                style={{
-                  flexDirection: 'row',
-                  justifyContent: 'flex-start',
-                  alignItems: 'center',
-                  columnGap: 10,
-                }}>
-                <View
-                  style={{
-                    width: 100,
-                  }}>
-                  <Text>Data 1</Text>
-                </View>
+          <TouchableOpacity>
+            <Text
+              style={{
+                color: 'lightblue',
+              }}>
+              1
+            </Text>
+          </TouchableOpacity>
 
-                <View
-                  style={{
-                    width: 100,
-                  }}>
-                  <Text>Data 2</Text>
-                </View>
-              </View>
-            </View>
-          </ScrollView>
+          <TouchableOpacity>
+            <Text
+              style={{
+                color: '#ccc',
+              }}>
+              2
+            </Text>
+          </TouchableOpacity>
+
+          <Text>..</Text>
+
+          <TouchableOpacity>
+            <Text
+              style={{
+                color: '#ccc',
+              }}>
+              10
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+            <Text>⇾</Text>
+          </TouchableOpacity>
         </View>
       </View>
-
-      {/* pagination */}
-    </View>
+    </ScrollView>
   );
 };
 
